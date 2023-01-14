6a4669a628f8de8a503599a238c0199e718f0065
Adnan Ademovic <adnanademovic100@gmail.com>
2023-01-14 14:15:28 +0100

    Implement nice debug print for sim state

--- examples/iracing_data_retrieval.rs
+++ examples/iracing_data_retrieval.rs
@@ -7,30 +7,15 @@ async fn main() {
     let mut client = Client::connect().await;
     println!("Connected to memory interface!");
     loop {
-        let sim_state;
-        loop {
-            if let Some(s) = client.wait_for_sim_state(Duration::from_millis(1000)) {
-                sim_state = s;
-                break;
-            } else {
-                println!("Waiting for iRacing data...");
-            }
+        while client
+            .wait_for_sim_state(Duration::from_millis(1000))
+            .is_none()
+        {
+            println!("Waiting for iRacing data...");
         }
-        for (key, val) in sim_state.variables() {
-            println!("{}: {:?}", key, val);
-        }
-        if let Ok(session_info) = sim_state.get_session_info_string() {
-            println!("Session info: {}", session_info);
-        }
-        println!("Received!");
         while client.is_connected() {
-            if client
-                .wait_for_sim_state(Duration::from_millis(16))
-                .is_some()
-            {
-                println!("Received new data");
-            } else {
-                println!("Waiting for further data");
+            if let Some(sim_state) = client.wait_for_sim_state(Duration::from_millis(16)) {
+                println!("{sim_state:#?}");
             }
         }
     }
--- src/iracing/sim_state.rs
+++ src/iracing/sim_state.rs
@@ -1,17 +1,33 @@
 use crate::iracing::string_decoding::cp1252_to_string;
-use crate::iracing_basic_solution::header::{Header, VarHeader, VarHeaders};
-use crate::iracing_basic_solution::VarData;
+use crate::iracing_basic_solution::header::{Header, VarHeader, VarHeaders, VarType};
+use crate::iracing_basic_solution::{Value, VarData};
 use anyhow::{bail, Result};
+use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
 use yaml_rust::{Yaml, YamlLoader};
 
 // TODO: implement debug that's aware of raw data content
 #[derive(Clone)]
 pub struct SimState {
-    _header: Header,
+    header: Header,
     variables: Arc<VarHeaders>,
     raw_data: Vec<u8>,
     raw_session_info: Vec<u8>,
+}
+
+impl Debug for SimState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SimState")
+            .field("header", &self.header)
+            .field(
+                "session_info",
+                &self
+                    .get_session_info_string()
+                    .unwrap_or_else(|_| format!("invalid data: {:?}", self.raw_session_info)),
+            )
+            .field("data", &DataDebugPrinter(self))
+            .finish()
+    }
 }
 
 impl SimState {
@@ -22,7 +38,7 @@ impl SimState {
         raw_session_info: Vec<u8>,
     ) -> Self {
         Self {
-            _header: header,
+            header,
             variables,
             raw_data,
             raw_session_info,
@@ -61,4 +77,49 @@ impl SimState {
         }
         Ok(items.swap_remove(0))
     }
+}
+
+struct DataDebugPrinter<'a>(&'a SimState);
+
+impl<'a> Debug for DataDebugPrinter<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use itertools::Itertools;
+        f.debug_list()
+            .entries(self.0.variables().values().map(|header| {
+                Variable {
+                    name: &header.name,
+                    description: &header.desc,
+                    datatype: header.var_type,
+                    unit: &header.unit,
+                    count_as_time: header.count_as_time,
+                    data: (0..header.count)
+                        .map(|idx| {
+                            format!(
+                                "{}",
+                                self.0
+                                    .read_at::<Value>(idx, header)
+                                    .unwrap_or(Value::Char(b'?'))
+                            )
+                        })
+                        .join(", "),
+                }
+            }))
+            .finish()
+    }
+}
+
+#[derive(Debug)]
+struct Variable<'a> {
+    #[allow(dead_code)]
+    name: &'a str,
+    #[allow(dead_code)]
+    description: &'a str,
+    #[allow(dead_code)]
+    datatype: VarType,
+    #[allow(dead_code)]
+    unit: &'a str,
+    #[allow(dead_code)]
+    count_as_time: bool,
+    #[allow(dead_code)]
+    data: String,
 }
